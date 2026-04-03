88f93026fe9354c5759c7416bf0b3e75606097cf
Rivavainfo <rivavainfo@gmail.com>
2026-04-03 19:57:03 +0530

    Add Google Services plugin to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false
